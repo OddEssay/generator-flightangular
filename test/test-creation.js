025b83b0ef069ce4ef9392136422acd1c466d2b8
Paul Freeman <paul@mojo=projects.com>
2013-09-09 14:51:36 +0000

    Improve list of files we expect to be created

--- test/test-creation.js
+++ test/test-creation.js
@@ -6,33 +6,39 @@ var helpers = require('yeoman-generator').test;
 
 
 describe('flightangular generator', function () {
-  beforeEach(function (done) {
-    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
-      if (err) {
-        return done(err);
-      }
+	beforeEach(function (done) {
+		helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+			if (err) {
+				return done(err);
+			}
 
-      this.app = helpers.createGenerator('flightangular:app', [
-        '../../app'
-      ]);
-      done();
-    }.bind(this));
-  });
-
-  it('creates expected files', function (done) {
-    var expected = [
-      // add files you expect to exist here.
-      'public/index.php',
-    ];
-
-    helpers.mockPrompt(this.app, {
-      'installBootstrap': true,
-		'installFontAwesome': true
-    });
-    this.app.options['skip-install'] = true;
-    this.app.run({}, function () {
-      helpers.assertFiles(expected);
-      done();
-    });
-  });
+			this.app = helpers.createGenerator('flightangular:app', [
+				'../../app'
+			]);
+			done();
+		}.bind(this));
+	});
+	it('creates expected files', function (done) {
+		var expected = [
+			// add files you expect to exist here.
+			'public/index.php',
+			'public/partials/index.html',
+			'js-src/app.js',
+			'Gruntfile.js',
+			'package.json',
+			'bower.json',
+			'composer.json',
+			'server/views/app.php',
+			'server/views/layout.php'
+		];
+		helpers.mockPrompt(this.app, {
+			'installBootstrap': true,
+			'installFontAwesome': true
+		});
+		this.app.options['skip-install'] = true;
+		this.app.run({}, function () {
+			helpers.assertFiles(expected);
+			done();
+		});
+	});
 });
